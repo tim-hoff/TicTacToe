cb5fddef5fc13c6bb64b1d041b1b31d2f6bb76e3
awesomegamer <avay_raul@hotmail.com>
2013-04-22 17:52:01 -0500

    fixed the NullPointerException

--- src/com/example/tictactoetitan/MainActivity.java
+++ src/com/example/tictactoetitan/MainActivity.java
@@ -33,15 +33,15 @@ public class MainActivity extends Activity {
 	{
 		tictac = new int[3][3];
 		
-		btn1 = (TicTacButton) findViewById(R.id.button1);
-		btn2 = (TicTacButton) findViewById(R.id.button2);
-		btn3 = (TicTacButton) findViewById(R.id.button3);
-		btn4 = (TicTacButton) findViewById(R.id.button4);
-		btn5 = (TicTacButton) findViewById(R.id.button5);
-		btn6 = (TicTacButton) findViewById(R.id.button6);
-		btn7 = (TicTacButton) findViewById(R.id.button7);
-		btn8 = (TicTacButton) findViewById(R.id.button8);
-		btn9 = (TicTacButton) findViewById(R.id.button9);
+		btn1 = (TicTacButton) findViewById(R.id.button1); btn1.set_x(0); btn1.set_y(0);
+		btn2 = (TicTacButton) findViewById(R.id.button2); btn2.set_x(0); btn2.set_y(1);
+		btn3 = (TicTacButton) findViewById(R.id.button3); btn3.set_x(0); btn3.set_y(2);
+		btn4 = (TicTacButton) findViewById(R.id.button4); btn4.set_x(1); btn4.set_y(0);
+		btn5 = (TicTacButton) findViewById(R.id.button5); btn5.set_x(1); btn5.set_y(1);
+		btn6 = (TicTacButton) findViewById(R.id.button6); btn6.set_x(1); btn6.set_y(2);
+		btn7 = (TicTacButton) findViewById(R.id.button7); btn7.set_x(2); btn7.set_y(0);
+		btn8 = (TicTacButton) findViewById(R.id.button8); btn8.set_x(2); btn8.set_y(1);
+		btn9 = (TicTacButton) findViewById(R.id.button9); btn9.set_x(2); btn9.set_y(2);
 		
 		message = (TextView) findViewById(R.id.textView1);
 		
@@ -50,9 +50,9 @@ public class MainActivity extends Activity {
 	
 	public static void changeState(int x, int y)
 	{
-		state = (state == State.circle) ? State.cross : State.circle;
-//		tictac[x][y] = (state == State.circle) ? 0 : 1;
+		tictac[x][y] = (state == State.circle) ? 0 : 1;
 		message.setText(state.toString() + " at " + x + "," + y);
+		state = (state == State.circle) ? State.cross : State.circle;
 	}
 	
 	public void gameStateCheck()
